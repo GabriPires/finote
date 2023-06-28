6a9cfc2b6cdf4d41d5d928500bdaa0feeb1eb6b3
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-28 19:26:03 -0300

    refactor(styles): header styles changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Wallet } from 'lucide-react'
 export function Header() {
   return (
     <header className="flex flex-col gap-4 lg:gap-8">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between rounded bg-base-200 p-4 shadow-lg">
         <Wallet className="h-10 w-10" />
         <button className="btn-primary btn rounded-full">Entrar</button>
       </div>
