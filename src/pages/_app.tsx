536f28260140e11476703f320fbb50db2be16efd
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-28 18:21:19 -0300

    feat(seo): configure application title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import { NextSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="Finote | Descomplicando suas finanças pessoais"
+        description="Finote é uma aplicação que te ajuda a descomplicar suas finanças pessoais, de forma simples e intuitiva."
+        // canonical="https://finote.vercel.app/"
+        openGraph={{
+          // url: 'https://finote.vercel.app/',
+          title: 'Finote | Descomplicando suas finanças pessoais',
+          description:
+            'Finote é uma aplicação que te ajuda a descomplicar suas finanças pessoais, de forma simples e intuitiva.',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
