de5bab870a3359cd7c60a00a5cccf31d90ec1c48
Gabriel Pires <gabpiresdev@gmail.com>
2023-06-28 18:08:10 -0300

    chore(styles): install and set up daisy ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button className="btn">Criar nota</button>
+    </div>
+  )
 }
